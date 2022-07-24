f76f0061e79b18fbda8ed81267159cef3132d882
Nazareth Villa <vnazareth6@gmail.com>
2022-07-24 08:45:06 -0500

    basic search engine

--- src/Search.js
+++ src/Search.js
@@ -3,21 +3,27 @@ import React, { useState } from "react";
 export default function Search() {
   let [word, setword] = useState(" ");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${word}`);
+  }
+
   function updateWord(event) {
     setword(event.target.value);
   }
 
   return (
     <div className="input-group rounded">
-      <input
-        type="search"
-        placeholder="Search"
-        onSubmit={updateWord}
-        className="form-control rounded"
-        aria-label="Search"
-        aria-describedby="search-addon"
-      />
-      <p>{word}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={updateWord}
+          className="form-control rounded"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        />
+      </form>
     </div>
   );
 }
