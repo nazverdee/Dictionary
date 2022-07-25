550904e503b1a64991f895d573fa7fccc46720e8
Nazareth Villa <vnazareth6@gmail.com>
2022-07-25 02:48:11 -0500

    Results section

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <section>
+          <h2>{props.results.word}</h2>
+          <Phonetic phonetic={props.results.phonetics[0]} />
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meaning meaning={meaning} />
-            </div>
+            </section>
           );
         })}
       </div>
