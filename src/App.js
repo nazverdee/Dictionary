acf726be39364da16fbd507074f4290c44294127
Nazareth Villa <vnazareth6@gmail.com>
2022-07-25 02:49:39 -0500

    App section

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <a href="/">
+    <div className="App container" id="container">
+      <a href="https://www.shecodes.io/">
         <img className="logo" src={logo} alt="logo" />
       </a>
-      <Dictionary />
+      <Dictionary defaultKeyword="empathy" />
     </div>
   );
 }
