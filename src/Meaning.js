b838c0a8cf47ab02ef826d0517e46b0337232c2b
Nazareth Villa <vnazareth6@gmail.com>
2022-07-25 02:48:32 -0500

    Meaning section

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,17 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>;
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <div className="definition"> {definition.definition} </div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
